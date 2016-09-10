994a01a9eafd1f8c630ccbd9631e1cc99ef3e762
Stefan Oury <stefan@intago.be>
2016-09-10 11:23:30 +0200

    added compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/*.coffee'];
 var jsSources = ['components/scripts/*.js'];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function () {
     gulp.src(coffeeSources)
@@ -15,8 +17,22 @@ gulp.task('coffee', function () {
 });
 
 
-gulp.task('js', function (){
+gulp.task('js', function () {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
+        .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
-})
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded',
+      comments: true,
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/css'))
+});
+
