fb16b10c774e42b73b76de9eb10e9f1da8aa4e61
Stefan Oury <stefan@intago.be>
2016-09-11 11:48:47 +0200

    added minified

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 
 var env,
@@ -60,8 +61,7 @@ gulp.task('compass', function() {
     .pipe(compass({
       sass: 'components/sass',
       image: outputDir + 'images',
-      style: sassStyle,
-      comments: true
+      style: sassStyle
     })
     .on('error', gutil.log))
     .pipe(gulp.dest(outputDir + 'css'))
@@ -72,7 +72,7 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
-  gulp.watch(htmlSources, ['html']);
+  gulp.watch('builds/development/*.html', ['html']);
   gulp.watch(jsonSources, ['json']);
 });
 
@@ -84,7 +84,9 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-  gulp.src(htmlSources)
+  gulp.src('builds/development/*.html')
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
     .pipe(connect.reload())
 });
 
